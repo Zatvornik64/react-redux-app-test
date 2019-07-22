f95f9fa9b323af12b15097efc4b6167b49a6c54c
Zatvornik64 <zatvornik@inbox.ru>
2019-07-22 16:09:41 +0400

    temp

---
+++ src/containers/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { connect } from 'react-redux'
+
+import './App.css';
+import { ItemsList } from '../components/ItemsList'
+import { Card } from '../components/Card'
+import {items, visible} from '../components/ItemData.js'
+
+class App extends React.Component {
+  /*state = {mainVisible: visible}
+
+  changeHandler = () => {
+    this.setState({mainVisible: visible})
+  }*/
+
+render () {
+  return (
+      <React.Fragment>
+        { this.state.mainVisible.list && <ItemsList onMenu={this.changeHandler} data={items}  />}
+        { this.state.mainVisible.card && <Card onMenu={this.changeHandler} data={items} />}
+      </React.Fragment>
+    )
+  }
+}
+
+const mapStateToProps = store => {
+  return {
+    mainVisible: store.visible
+  }
+}
+const mapDispatchToProps = dispatch => {
+  return {
+    setVisibleAction: visible => dispatch (setVisible(visible)),
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+export const initialState = {
+    visible = {
+        list: true,
+        card: false,
+      }
+}
+
+export function rootReducer (sate = initialState) {
+    return state
+}
